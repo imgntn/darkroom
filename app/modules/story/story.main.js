93e59f15392388e80a134ea44a2fc720ad849469
ffpaladin <ffpaladin@gmail.com>
2014-01-25 10:38:14 -0800

    new story files

---
+++ app/modules/story/story.main.js
@@ -0,0 +1,52 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var $ = require("jquery");
+  var _ = require("underscore");
+  var Backbone = require("backbone");
+  var app = require("app");
+
+
+var Story={
+	introText:"I can't seem to find it. What did I do today?...",
+	//makeCoolText:function(){
+	//},
+
+	storyLines: new Array(),
+	dreamQuotes: new Array(),
+	makeTheStory:function(){
+		this.storyLines[0] = {
+			text:'I got out of bed. ',
+			tag:sometag
+		}
+		
+		this.storyLines[1] = {
+			text:'For breakfast, I made myself a spinach and cheese omlete.',
+			tag:sometag
+		}
+		
+		this.storyLines[2] = {
+			text:'I walked the dog.',
+			tag:sometag
+		}
+		
+		this.storyLines[3] = {
+			text:'Then I went to get some coffee.',
+			tag:sometag
+		}
+
+		// --------------------
+
+		this.dreamQuotes[0] = "dreams were related to the world of the supernatural beings in whom they believed, and that they brought inspirations from the gods and demons."
+
+		this.dreamQuotes[1] = "dreams must serve a special purpose in respect of the dreamer; that, as a rule, they predicted the future."
+
+		this.dreamQuotes[2] = "the dream is not god-sent, that it is not of divine but of demonic origin."
+
+
+	}
+
+}
+
+module.exports=Story
+});
