502731477ce9fe846301abba11774f7512b3b2e2
James B. Pollack, MFA <james.b.pollack@gmail.com>
2025-06-05 18:55:35 -0700

    Update dependencies with npm-check-updates

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,9 +1,9 @@
 import { playwrightLauncher } from '@web/test-runner-playwright';
-import { jasmine } from '@web/test-runner-jasmine';
+import { jasmineTestRunnerConfig } from 'web-test-runner-jasmine';
 
 export default {
+  ...jasmineTestRunnerConfig(),
   files: 'test/jasmine/specs/**/*.spec.js',
   nodeResolve: true,
   browsers: [playwrightLauncher({ product: 'chromium' })],
-  plugins: [jasmine()],
 };
